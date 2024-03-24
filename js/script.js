cf5ba3cc57d99a5b97e47acebdab8ecccc8a6f4d
Jhenia <36773626+Jhenia@users.noreply.github.com>
2024-03-24 13:37:42 +0200

    last commit

--- js/script.js
+++ js/script.js
@@ -130,7 +130,7 @@ function isElementInViewport(el) {
   return (
     rect.top >= 0 &&
     rect.left >= 0 &&
-    rect.bottom -450 <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.bottom -500 <= (window.innerHeight || document.documentElement.clientHeight) &&
     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
   );
 }
